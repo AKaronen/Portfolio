21906009bb6a549d412faa957b440cf48b00a74d
Unlucky72 <alex.karonen@gmail.com>
2022-09-12 14:46:52 +0300

    nav for desktop done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,28 @@
-// DOM elements 
+// DOM elements
 
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
 
-const Nav = document.querySelector('.menu-nav');
+const Nav = document.querySelector(".menu-nav");
 
-const menuBranding = document.querySelector('.menu-branding');
-
-const menuItems = document.querySelectorAll('.nav-item');
+const menuItems = document.querySelectorAll(".nav-item");
 
 let menuOpen = false;
 
-
-
 const toggleMenu = () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menu.classList.add("show");
+    Nav.classList.add("show");
+    menuItems.forEach((item) => item.classList.add("show"));
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menu.classList.remove("show");
+    Nav.classList.remove("show");
+    menuItems.forEach((item) => item.classList.remove("show"));
+    menuOpen = false;
+  }
+};
 
-    if(!menuOpen){
-        menuBtn.classList.add('open');
-        menu.classList.add('show');
-        Nav.classList.add('show');
-        menuBranding.classList.add('show');
-        menuItems.forEach(item => item.classList.add('show'));
-        menuOpen = !menuOpen;
-    }else{
-        menuBtn.classList.remove('open');
-        menu.classList.remove('show');
-        Nav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        menuItems.forEach(item => item.classList.remove('show'));
-        menuOpen = !menuOpen; 
-    }
-}
-
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
